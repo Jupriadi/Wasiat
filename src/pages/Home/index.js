ebeb1d8a99de8012cfd9671adb0bf0db4a610973
Jupriadi <jpashter@gmail.com>
2020-08-11 06:47:24 +0700

    memodifikasi file index di surah dan di home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -17,6 +17,7 @@ const Home =({navigation,id})=>{
         <View  style={styles.container}>
             <TopNav/>
             <Text style={styles.title}>{id}</Text>
+            
             <DaftarIsi id={id} onPress={()=>GoTo('Surah')}/>
         </View>
     )
--- src/pages/Surah/index.js
+++ src/pages/Surah/index.js
@@ -1,10 +1,34 @@
-import React, { Component } from 'react';
-import {  View, Text } from 'react-native';
+import React from 'react';
+import { Text, FlatList,SafeAreaView, View, Image,StyleSheet } from 'react-native';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+import DATA from '../../assets/Data.js';
 
-
-const Surah = () => {
+const Item = ({ judul,id}) => {
+    var pict = ("../../assets/img/Text/"+id+".png");
     return(
-        <Text>Surah</Text>
+        <TouchableOpacity style={styles.item} key={id}>
+            <Image style={styles.img} source={{uri:pict}} />
+            <Text>{pict}</Text>
+        </TouchableOpacity>
+    )
+  };
+  
+const Surah = () => {
+    const renderItem = ({ item}) => (
+        <Item judul={item.judul} id={item.id}  />
+
+      );
+
+    return(
+        <View style={{flex:1}}>
+            <SafeAreaView>
+                <FlatList
+                    data={DATA}
+                    renderItem={renderItem}
+                    keyExtractor={item => item.id }
+                />
+            </SafeAreaView>      
+        </View>
     )
 }
 
@@ -13,5 +37,18 @@ const Surah = () => {
     
 // )
 // }
+
+const styles=StyleSheet.create({
+    img:{
+        width:"100%",
+        resizeMode: "contain",
+    },
+    // flatlist
+    item: {
+        marginHorizontal: 16,
+       padding:8, borderBottomColor:"grey",borderBottomWidth:1,
+      },
+})
+
 
 export default Surah;
